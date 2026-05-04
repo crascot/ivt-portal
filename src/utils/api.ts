0e22f01feb771f4bcd15ffbdcc9dcc64da596024
Yaroslav <yarsitdikov@gmail.com>
2026-05-04 12:53:15 +0600

    new

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:8034',
+  baseURL: 'http://95.217.88.31:8034',
   timeout: 10000,
 });
 
